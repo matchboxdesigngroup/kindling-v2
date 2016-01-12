126d1ea85330e77215e839ea10cb4877aa58c489
Cullen Whitmore <cwhitmore@matchboxdesigngroup.com>
2016-01-12 03:04:42 -0600

    Filters Gravity Forms CSS classes for a field

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,25 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+
+/**
+ * Filters Gravity Forms CSS classes for a field.
+ *
+ * @link    http://www.gravityhelp.com/documentation/page/Gform_field_css_class
+ *
+ * @param   string  $classes  The CSS classes to be filtered, separated by empty spaces (i.e. "gfield custom_class").
+ * @param   array   $field    Current field.
+ * @param   array   $form     Current form.
+ *
+ * @return  string            The filtered CSS classes separated by empty spaces (i.e. "gfield custom_class").
+ */
+if ( ! function_exists( 'sn_gforms_input_class' ) ) {
+function sn_gforms_input_class( $classes, $field, $form ){
+  $input_slug = sanitize_title( $field['label'] );
+  $classes   .= " gfield-{$input_slug}";
+
+  return $classes;
+} // sn_gforms_input_class
+add_action( 'gform_field_css_class', 'sn_gforms_input_class', 10, 3 );
+} // if()
