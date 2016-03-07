cc5a7d36cc9211a9ce594ef6dd5bee2dbfcb7c83
Dan Holloran <dholloran@matchboxdesigngroup.com>
2016-03-07 13:56:45 -0600

    Sets gform field class to be more robust

--- lib/extras.php
+++ lib/extras.php
@@ -45,8 +45,30 @@ add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
  * @return  string            The filtered CSS classes separated by empty spaces (i.e. "gfield custom_class").
  */
 add_action( 'gform_field_css_class', function( $classes, $field, $form ){
-  $input_slug = sanitize_title( $field['label'] );
-  $classes   .= " gfield-{$input_slug}";
+  // Add the field label class.
+  $label       = ( isset( $field['label'] ) ) ? $field['label'] : '';
+  $admin_label = ( isset( $field['adminLabel'] ) ) ? $field['adminLabel'] : '';
+  $input_slug  = ( '' === $admin_label ) ? $label : $admin_label;
+  $input_slug  = sanitize_title( $input_slug );
+  $classes    .= " gfield-{$input_slug}";
+
+  // Add the field type class.
+  $type     = ( isset( $field['type'] ) ) ? $field['type'] : '';
+  $type     = sanitize_title( $type );
+  $classes .= " gfield-type-{$type}";
+
+  // Add placeholder classes.
+  $has_placeholder = ( isset( $field['placeholder'] ) and '' !== $field['placeholder'] );
+  if ( isset( $field['inputs'] ) ) {
+    foreach ( $field['inputs'] as $input ) {
+      if ( isset( $input['placeholder'] ) and '' !== $input['placeholder'] ) {
+        $has_placeholder = true;
+
+        break;
+      } // if()
+    } // foreach()
+  } // if()
+  $classes .= ( $has_placeholder ) ? ' gfield-placeholder' : '';
 
   return $classes;
 }, 10, 3 );
