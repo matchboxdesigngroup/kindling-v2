0d763eda2018b93abd70868ec67dab33d87a5763
Dan Holloran <dholloran@matchboxdesigngroup.com>
2016-03-22 14:43:10 -0500

    Removes extra methods held over from old post meta

--- lib/post-types/type-page.php
+++ lib/post-types/type-page.php
@@ -1,36 +1,22 @@
 <?php
 /**
  * Kindling Type Page Class.
- */
-
-
-
-/**
- * Handles anything custom for the default WordPress "post" post type.
  *
- * @package      WordPress
- * @subpackage   Kindling
- *
+ * @package WordPress
+ * @subpackage Kindling
  * @author       Matchbox Design Group <info@matchboxdesigngroup.com>
  */
+
+/**
+ * Handles anything custom for the default WordPress "page" post type.
+ */
 class MDG_Type_Page extends MDG_Type_Base {
-	/**
-	 * Holds all the transient keys for the cache.
-	 *
-	 * @param  array
-	 */
-	public $type_transients = array();
-
-
-
 	/**
 	 * The slug of the post types landing page if not post type archive.
 	 *
 	 * @var  string
 	 */
 	public $landing_page_slug = '';
-
-
 
 	/**
 	 * The slug of the post types landing page template if not post type archive.
@@ -39,146 +25,36 @@ class MDG_Type_Page extends MDG_Type_Base {
 	 */
 	public $landing_page_template = '';
 
-
-
 	/**
 	 * Class constructor, handles instantiation functionality for the class
 	 */
 	function __construct() {
-		/** @var string  REQUIRED slug for post type */
-		$this->post_type        = 'page';
+		/**
+		 * REQUIRED slug for post type.
+		 *
+		 * @var string
+		 */
+		$this->post_type = 'page';
 
-		/** @var string  REQUIRED title of post type */
-		$this->post_type_title  = 'Pages';
+		/**
+		 * REQUIRED title of post type.
+		 *
+		 * @var string
+		 */
+		$this->post_type_title = 'Pages';
 
-		/** @var string  REQUIRED singular title */
+		/**
+		 * REQUIRED singular title.
+		 *
+		 * @var string
+		 */
 		$this->post_type_single = 'Page';
-
-		// MDG_Meta_Helper Properties
-		$this->_set_mdg_meta_helper_options();
 
 		parent::__construct();
 	} // __construct()
 
-
-
 	/**
-	 * Handles setting of the optional properties of MDG_Meta_Helper
-	 *
-	 * return Void
-	 */
-	private function _set_mdg_meta_helper_options() {
-		/** @var array Meta box id(s) to be removed */
-		$this->meta_boxes_to_remove = array(
-			// array(
-			//  'id'      => 'authordiv',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'categorydiv',
-			//  'context' => 'side',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => "{$this->post_type}-categorydiv",
-			//  'context' => 'side',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'commentstatusdiv',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'commentsdiv',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'formatdiv',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'pageparentdiv',
-			//  'context' => 'side',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'postexcerpt',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'revisionsdiv',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'slugdiv',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'submitdiv',
-			//  'context' => 'side',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'tagsdiv-post_tag',
-			//  'context' => 'side',
-			//  'page'    => $this->post_type,
-			// ),
-			// array(
-			//  'id'      => 'trackbacksdiv',
-			//  'context' => 'normal',
-			//  'page'    => $this->post_type,
-			// ),
-		);
-	} // _set_mdg_meta_helper_options()
-
-
-	/**
-	 * Creates custom meta fields array.
-	 *
-	 * @return ArrayObject Custom meta fields
-	 */
-	public function get_custom_meta_fields() {
-		$meta_fields = array();
-
-		if ( ! $this->is_current_post_type() ) {
-			return $meta_fields;
-		} // if()
-
-		return $meta_fields;
-	} // get_custom_meta_fields()
-
-
-
-	/**
-	 * Creates custom meta fields array for display after the post title.
-	 * Note: probably only should be textual inputs...
-	 *
-	 * @since   0.2.3
-	 *
-	 * @return  array Custom meta fields
-	 */
-	public function get_custom_after_title_meta_fields() {
-		$meta_fields = array();
-
-		if ( ! $this->is_current_post_type() ) {
-			return $meta_fields;
-		} // if()
-
-		return $meta_fields;
-	} // get_custom_after_title_meta_fields()
-
-
-
-
-	/**
-	 * Disables creating post type since post is a default post type
+	 * Disables creating post type since post is a default post type.
 	 */
 	public function register_post_type() {}
 } // END Class MDG_Type_Page()
