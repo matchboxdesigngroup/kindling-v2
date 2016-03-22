ac41066771627b9bc5ace0d8cdc7b6b3a1d10f48
Danny Holloran <DTHolloran@Gmail.com>
2016-03-22 14:03:21 -0500

    Adds theme version for theme assets closes #10

--- lib/setup.php
+++ lib/setup.php
@@ -98,12 +98,15 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+  $theme         = wp_get_theme();
+	$theme_version = $theme->get( 'Version' );
+	
+  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, $theme_version);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], $theme_version, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
