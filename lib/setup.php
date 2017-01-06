5817c9c1f7ef4a6375a7096ca0cd83267220be27
Dan Holloran <dholloran@matchboxdesigngroup.com>
2017-01-06 15:49:17 -0600

    Removes footer sidebar register

--- lib/setup.php
+++ lib/setup.php
@@ -65,15 +65,6 @@ function widgets_init() {
     'before_title'  => '<h3>',
     'after_title'   => '</h3>'
   ]);
-
-  register_sidebar([
-    'name'          => __('Footer', 'sage'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
 
