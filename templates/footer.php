5ac09c4bd5445cdc020e399e94f92c0b8e29fff6
Dan Holloran <dholloran@matchboxdesigngroup.com>
2017-01-06 14:27:58 -0600

    Adds copyright

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,10 @@
+<?php
+$copyright_name = get_bloginfo( 'name' );
+$copyright_year = date( 'Y' );
+?>
 <footer class="content-info">
   <div class="container">
+  	<span class="copyright">&copy; <?php esc_attr_e( "{$copyright_year} {$copyright_name}" ); ?></span>
 	<?php
 	if ( has_nav_menu( 'footer_navigation' ) ) {
 		wp_nav_menu( [
