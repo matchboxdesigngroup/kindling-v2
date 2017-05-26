9c15b4356d64546e519a936083ffae7e024db497
Dan Holloran <dtholloran@gmail.com>
2017-05-26 07:08:40 -0500

    Adds shared Composer plugin

---
+++ wp-content/plugins/kindling-packages/kindling-packages.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Theme Packages
+Description: Composer packages and auto-loading for theme/plugins.
+Plugin URI: http://matchboxdesigngroup.com/
+Author: Matchbox Design Group
+Author URI: http://matchboxdesigngroup.com/
+Version: 1.0.0
+License: GPL2
+*/
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+do_action('kindling_packages_ready');
