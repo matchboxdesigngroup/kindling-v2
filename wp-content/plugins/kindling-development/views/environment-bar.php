d68d3fb2f658723b73dc34f63b586439438d8945
Dan Holloran <dtholloran@gmail.com>
2017-05-26 19:49:59 -0500

    Fixes debug prefixes

--- wp-content/plugins/kindling-development/views/environment-bar.php
+++ wp-content/plugins/kindling-development/views/environment-bar.php
@@ -2,9 +2,9 @@
 $message = ucwords(kdev_get_environment_type());
 $wordsLimit = 360 / strlen($message);
 ?>
-<div class="mdg-environment-bar">
+<div class="kdev-environment-bar">
     <?php for ($i = 0; $i < $wordsLimit; $i++) { ?>
-    <div class="mdg-environment-bar-message">
+    <div class="kdev-environment-bar-message">
         <?php echo wp_kses_post($message); ?>
     </div>
     <?php } ?>
