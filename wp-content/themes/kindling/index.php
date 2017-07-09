25b332549a9f164fca92e6e0fd4bc54462e2fff5
Dan Holloran <dtholloran@gmail.com>
2017-07-08 20:19:07 -0500

    Adds an empty index.php so wordpress is happy :)

--- wp-content/themes/kindling/index.php
+++ wp-content/themes/kindling/index.php
@@ -1,10 +1,3 @@
 <?php
-/**
- * Default template.
- *
- * @package Kindling_Theme
- * @author  Matchbox Design Group <info@matchboxdesigngroup.com>
- */
 
-get_template_part('templates/partials/page-header');
-get_template_part('templates/content/news');
+// This file let intentionally blank
