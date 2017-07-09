3d8960872d8f5f90c0e7033b23e9a5759dedb365
Dan Holloran <dtholloran@gmail.com>
2017-07-09 08:03:40 -0500

    Converts post type to use IOC container

--- wp-content/mu-plugins/kindling-post-types/libs/actions.php
+++ wp-content/mu-plugins/kindling-post-types/libs/actions.php
@@ -6,19 +6,31 @@
  * @author  Matchbox Design Group <info@matchboxdesigngroup.com>
  */
 
+use Illuminate\Contracts\Container\Container as ContainerContract;
+
 /**
  * Register post types.
  * Add fully name spaced classes to `kpt_get_type_classes()` or use the kpt_type_classes filter.
  * All post classes must extend Kindling\PostTypes\Base see src/Type/Stub.php for an example.
  */
 add_action('init', function () {
-    global $kptTypes;
+    kpt_get_type_classes()->each(function ($class) {
+        if (!class_exists($class)) {
+            return false;
+        }
 
-    $kptTypes = kpt_get_type_classes()->map(function ($class) {
-        return class_exists($class) ? (new $class)->init() : false;
-    })->filter(function ($item) {
-        return $item;
-    })->keyBy(function ($type) {
-        return $type->postType;
+        // Add the post type to the container.
+        $abstract = explode('\\', $class);
+        $abstract = 'type.' . end($abstract);
+        kindling()->singleton($abstract, function (ContainerContract $app) use ($class) {
+            $class = new $class;
+
+            $class->init();
+
+            return $class;
+        });
+
+        // Resolve the post type out of the container to initialize it.
+        kindling($abstract);
     });
 });
--- wp-content/mu-plugins/kindling-post-types/libs/functions.php
+++ wp-content/mu-plugins/kindling-post-types/libs/functions.php
@@ -7,30 +7,6 @@
  */
 
 /**
- * Get all post type class instances.
- *
- * @return Illuminate\Support\Collection
- */
-function kpt_get_types()
-{
-    global $kptTypes;
-
-    return isset($kptTypes) ? $kptTypes : collect([]);
-}
-
-/**
- * Get an individual post type class instance.
- *
- * @param  string $type
- *
- * @return object|null
- */
-function kpt_get_type($type)
-{
-    return kpt_get_types()->get($type);
-}
-
-/**
  * Gets the fully name spaced post type classes.
  *
  * @return Illuminate\Support\Collection
@@ -41,5 +17,5 @@ function kpt_get_type_classes()
         // 'Kindling\PostTypes\Type\Stub',
         'Kindling\PostTypes\Type\Post',
         'Kindling\PostTypes\Type\Page',
-    ]));
+    ]))->unique();
 }
